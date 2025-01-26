db8daeb0e1735b405e7fd7b043bee37548c7da21
Jason Kneen <jason.kneen@bouncingfish.com>
2025-01-26 14:57:31 +0000

    Fixes

--- webview-ui/src/components/chat/Announcement.tsx
+++ webview-ui/src/components/chat/Announcement.tsx
@@ -12,6 +12,8 @@ interface AnnouncementProps {
 You must update the latestAnnouncementId in ClineProvider for new announcements to show to users. This new id will be compared with whats in state for the 'last announcement shown', and if it's different then the announcement will render. As soon as an announcement is shown, the id will be updated in state. This ensures that announcements are not shown more than once, even if the user doesn't close it themselves.
 */
 const Announcement = ({ version, hideAnnouncement }: AnnouncementProps) => {
+	const versionSegments = version.split(".")
+	const minorVersion = versionSegments[1]
 	return (
 		<div
 			style={{
